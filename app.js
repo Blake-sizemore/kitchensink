90a2f59b6ab815f368e3543c7b2fd7e630819886
Blake-sizemore <magnify1more@gmail.com>
2024-02-09 18:13:21 -0500

    final ts set up

--- app.js
+++ app.js
@@ -1,62 +1,54 @@
-var name = 'blake';
-const stateTotal = 50;
-let sum = 4 + 5;
+var nameIt = 'blake';
+var stateTotal = 50;
+var sum = 4 + 5;
 // adding comments for functionality
 function sayHello() {
-    alert('Hello World')
+    alert('Hello World');
 }
 sayHello();
 function checkAge(personName, personAge) {
     if (personAge < 21) {
-        alert("Sorry " + personName + ", you aren't old enough to view this page!")
+        alert("Sorry " + personName + ", you aren't old enough to view this page!");
     }
 }
-
 checkAge('Charlie', 21);
 checkAge("Abby", 27);
 checkAge("James", 18);
 checkAge("John", 17);
-
-let favVeg = ["tomatoe", "Squash", "Potatoe"];
-
-let petObj = {
+var favVeg = ["tomatoe", "Squash", "Potatoe"];
+var petObj = {
     name: "buddy",
     breed: "Ratterrier"
-}
-
+};
 console.log(petObj.name + " " + petObj.breed);
-
-let arrObj = [{
-    name: "buddy",
-    age: 23
-}, {
-    name: "willy",
-    age: 52
-}, {
-    name: "susan",
-    age: 13
-}, {
-    name: "tommy",
-    age: 21
-}, {
-    name: "whoever",
-    age: 15
+var arrObj = [{
+        name: "buddy",
+        age: 23
+    }, {
+        name: "willy",
+        age: 52
+    }, {
+        name: "susan",
+        age: 13
+    }, {
+        name: "tommy",
+        age: 21
+    }, {
+        name: "whoever",
+        age: 15
+    }
+];
+for (var id = 0; id < arrObj.length; id++) {
+    var element = arrObj[id];
+    checkAge(element.name, element.age);
 }
-]
-
-for (let id = 0; id < arrObj.length; id++) {
-    let element = arrObj[id];
-    checkAge(element.name,element.age);
-    
-}
-
-function getLength(check){
-    let checkLength = check.length;
-    if(checkLength%2){
-        console.log('The world is an odd place!')
-    } else {
-        console.log('The world is an nice and even!!')
+function getLength(check) {
+    var checkLength = check.length;
+    if (checkLength % 2) {
+        console.log('The world is an odd place!');
+    }
+    else {
+        console.log('The world is an nice and even!!');
     }
 }
-
 getLength('Hi');
---
+++ app.ts
@@ -0,0 +1,65 @@
+var nameIt:string = 'blake';
+var stateTotal: number = 50;
+var sum:number = 4 + 5;
+// adding comments for functionality
+
+function sayHello() {
+    alert('Hello World')
+}
+sayHello();
+
+function checkAge(personName:string, personAge:number): any ;
+function checkAge(personName, personAge) {
+    if (personAge < 21) {
+        alert("Sorry " + personName + ", you aren't old enough to view this page!")
+    }
+}
+
+checkAge('Charlie', 21);
+checkAge("Abby", 27);
+checkAge("James", 18);
+checkAge("John", 17);
+
+var favVeg: string[] = ["tomatoe", "Squash", "Potatoe"];
+
+var petObj = {
+    name: "buddy",
+    breed: "Ratterrier"
+}
+
+console.log(petObj.name + " " + petObj.breed);
+
+var arrObj = [{
+    name: "buddy",
+    age: 23
+}, {
+    name: "willy",
+    age: 52
+}, {
+    name: "susan",
+    age: 13
+}, {
+    name: "tommy",
+    age: 21
+}, {
+    name: "whoever",
+    age: 15
+}
+]
+
+for (let id = 0; id < arrObj.length; id++) {
+    let element = arrObj[id];
+    checkAge(element.name,element.age);
+    
+}
+
+function getLength(check){
+    let checkLength = check.length;
+    if(checkLength%2){
+        console.log('The world is an odd place!')
+    } else {
+        console.log('The world is an nice and even!!')
+    }
+}
+
+getLength('Hi');
